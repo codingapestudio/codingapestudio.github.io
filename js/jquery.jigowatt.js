3ca2fc344a4f9eb3740dada62c8bbe3be8ef5a9a
Codingape <barryjiao@Barrys-MBP.gateway>
2015-03-09 16:23:48 +1100

    11

--- js/jquery.jigowatt.js
+++ js/jquery.jigowatt.js
@@ -18,7 +18,7 @@ jQuery(document).ready(function(){
 			comments: $('#comments').val()
 		},
 			function(data){
-				document.getElementById('message').innerHTML = '<h3 class="text-align-center">We have received your message and we will get back to you as soon as possible!</h4>';
+				document.getElementById('message').innerHTML = '<h3 class="text-align-center" style="color:#fff;">We have received your message and we will get back to you as soon as possible!</h4>';
 				$('#message').slideDown('slow');
 				$('#contactform img.loader').fadeOut('slow',function(){$(this).remove()});
 				$('#submit').removeAttr('disabled');
